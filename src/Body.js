ebcb05b1b430e50b4d1c352a2d8a98ac76e3dfd4
Marcus Lampert <marcus.lampert@gmail.com>
2018-04-19 13:50:28 +0200

    Initial commit

---
+++ src/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Body extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Photos go here</h1>
+      </div>
+    );
+  }
+}
+
+export default Body;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Catharina Schimert</h1>
+        <h2>Photography</h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
